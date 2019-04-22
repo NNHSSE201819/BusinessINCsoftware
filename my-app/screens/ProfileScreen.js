a8bb4fa2f6d7fdc54adb76620b8df1b4833ae602
Jessica Liu <gjliu@stu.naperville203.org>
2019-04-22 14:23:34 -0500

    Restarting profile layout

--- my-app/screens/ProfileScreen.js
+++ my-app/screens/ProfileScreen.js
@@ -12,8 +12,10 @@ import {
   Picker,
   TouchableHighlight,
   Modal,
+  TextInput,
   TouchableOpacity,
 } from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
 const styles = StyleSheet.create({
   container: {
@@ -24,17 +26,22 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    backgroundColor: 'grey',
+    backgroundColor: '#EFE9E9',
     marginRight: 20,
     marginLeft: 20,
   },
   divider: {
-    height: 10,
-    backgroundColor: '#6f4141',
+    height: 5,
+    backgroundColor: '#6F4141',
+  },
+  subdivider: {
+    height: 3,
+    backgroundColor: '#6F4141',
   },
   profileHeader: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'flex-start',
     marginBottom: 25,
     marginTop: 75,
   },
@@ -51,14 +58,15 @@ const styles = StyleSheet.create({
   },
   textMain: {
     fontSize: 25,
-    fontWeight: 'bold',
     marginLeft: 20,
-    color: 'black'
+    fontFamily: 'HelveticaNeue-Light'
+
   },
   textLittle: {
     fontSize: 18,
     marginLeft: 20,
     marginRight: 20,
+    fontFamily: 'HelveticaNeue-Light'
   },
   hideModalText: {
     marginTop: 5,
@@ -67,21 +75,26 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     color: 'white',
     fontSize: 25,
-    fontWeight: 'bold'
+    fontFamily: 'HelveticaNeue-Light',
   }
 });
 
 export default class ProfileScreen extends Component {
   static navigationOptions = {
     header:null,
+    title: 'ProfileScreen',
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       modalMona: false,
       modalAnahita: false,
       modalLeilani: false,
+      insertName: 'Insert Name',
+      modalEmail: false,
+      modalBirthday: false,
+      email: 'gracieliu@gmail.com',
     }
   }
 
@@ -95,6 +108,9 @@ export default class ProfileScreen extends Component {
 
   meetLeilani(visible) {
     this.setState({modalLeilani: visible});
+  }
+  openEmail(visible) {
+    this.setState({modalEmail: visible});
   }
 
 
@@ -116,8 +132,8 @@ export default class ProfileScreen extends Component {
               style ={styles.iconImage}
               source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/your_profile_picture.jpg')}
             />
-            <Text style={{fontSize: 25, fontWeight: 'bold'}}>Mona Fang</Text>
-            <Text style={{textAlign: 'center',fontSize: 20, marginLeft: 15, marginRight: 15}}>
+            <Text style={{fontSize: 25, fontFamily: 'HelveticaNeue-Light'}}>Mona Fang</Text>
+            <Text style={{textAlign: 'center',fontSize: 20, marginLeft: 15, marginRight: 15, fontFamily: 'HelveticaNeue-Light'}}>
             is compelled to help solve the unsustainable problems of fashion.
             As a clothing sewing hobbyist, she enjoys designing and styling pieces of her own.
             Her personas range from laid-back to professional.
@@ -152,8 +168,8 @@ export default class ProfileScreen extends Component {
               style ={styles.iconImage}
               source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/anahita.jpg')}
             />
-            <Text style={{fontSize: 25, fontWeight: 'bold'}}>Anahita</Text>
-            <Text style={{textAlign: 'center',fontSize: 20, marginLeft: 15, marginRight: 15}}>
+            <Text style={{fontSize: 25, fontFamily: 'HelveticaNeue-Light'}}>Anahita</Text>
+            <Text style={{textAlign: 'center',fontSize: 20, marginLeft: 15, marginRight: 15, fontFamily: 'HelveticaNeue-Light'}}>
             is part of the circular fashion initiative because she really appreciates the notion that second-hand clothing has a rich history and the idea that when you trade clothing with someone, it’s as if you’re giving each other a piece of your histories. Her style is very preppy-chic, and she can usually be seen wearing dresses and skirts. Anahita’s style icon is young Brooke Shields, and she believes the most important fashion accessory is a killer pair of shoes and a matching confidence. Her vision for the future is one where people stop throwing their clothing away and start showing it some love.
             </Text>
             <TouchableHighlight
@@ -180,8 +196,8 @@ export default class ProfileScreen extends Component {
               style ={styles.iconImage}
               source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/leilani.jpg')}
             />
-            <Text style={{fontSize: 25, fontWeight: 'bold'}}>Leilani</Text>
-            <Text style={{textAlign: 'center',fontSize: 20, marginLeft: 15, marginRight: 15}}>
+            <Text style={{fontSize: 25, fontFamily: 'HelveticaNeue-Light'}}>Leilani</Text>
+            <Text style={{textAlign: 'center',fontSize: 20, marginLeft: 15, marginRight: 15, fontFamily: 'HelveticaNeue-Light'}}>
             is a young woman looking to make some sort of impact, whether it be big or small. She has always been interested in helping the environment throughout her life. As for her style, she wears pretty much anything that she feels comfortable in. To her, it doesn't matter where the clothing comes from as long as she feels confident and happy; that's all that matters to her. Style360 brings to life the idea that she can still wear clothing that she feels comfortable in and at the same time promote circular fashion.
             </Text>
             <TouchableHighlight
@@ -194,48 +210,87 @@ export default class ProfileScreen extends Component {
           </View>
         </Modal>
 
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalEmail}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');}}>
+          <View style={{flex:1, alignItems:'center', justifyContent:'center',backgroundColor: '#4CD4B2'}}>
+            <TextInput
+              style ={styles.hideModalText}
+              onChangeText={(insertEmail) => this.setState({email: insertEmail})}
+              value={this.state.email}
+            />
+            <TouchableHighlight
+              onPress={() => {
+                this.openEmail(!this.state.modalEmail)}}
+              style={{marginTop: 25, backgroundColor:'#008766'}}
+            >
+              <Text style={styles.hideModalText}>Close</Text>
+            </TouchableHighlight>
+          </View>
+        </Modal>
+
+{/*
+
+
+  Tabs, not modals
+
+
+*/}
+
 
         <View style ={styles.profileHeader}>
           <Image
             style ={styles.iconImage}
-            source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/style.png')}
-          />
+            source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/style.png')}/>
           <Text style ={styles.textMain}>
-            Insert Name Here
+            {this.state.insertName}
           </Text>
         </View>
 
         <View style ={styles.divider}/>
 
         <View style={styles.heading}>
+          <Text style ={styles.textMain}>Personal Info</Text>
+        </View>
+
+        <View style ={styles.subdivider}/>
+
+        <View style={{flexDirection:'row', alignItems: 'center', height: 40, backgroundColor:'#ccc3c3'}}>
+          <Text style ={styles.textMain}>Email</Text>
           <TouchableHighlight
-            onPress={() => {Alert.alert('Modal has been closed.')}}
-            style ={styles.button}
+            onPress={() => this.openEmail(true)}
+            style ={{flexDirection: 'row', justifyContent: 'center', flex: 1}}
           >
-            <Text style ={styles.textMain}>Personal Info</Text>
+            <View style={{justifyContent: 'flex-end',flexDirection:'row'}}>
+              <Text style ={{fontSize:20,fontFamily:'HelveticaNeue-Light'}}>{this.state.email}</Text>
+              <Text style ={{fontSize:20,fontFamily:'HelveticaNeue-Light'}}>  ></Text>
+            </View>
           </TouchableHighlight>
         </View>
 
-        <View style ={styles.divider}/>
+        <View style={styles.divider}/>
 
         <View style={styles.heading}>
-          <TouchableHighlight
-            onPress={() => {Alert.alert('Modal has been closed.')}}
+          <TouchableOpacity
+            onPress={() => {Alert.alert('Style Profile shows up here.')}}
             style ={styles.button}
           >
             <Text style ={styles.textMain}>Style Profile</Text>
-          </TouchableHighlight>
+          </TouchableOpacity>
         </View>
 
         <View style ={styles.divider}/>
 
         <View style={styles.heading}>
-          <TouchableHighlight
-            onPress={() => {Alert.alert('Modal has been closed.')}}
+          <TouchableOpacity
+            onPress={() => {Alert.alert('Transaction History shows up here.')}}
             style ={styles.button}
           >
             <Text style ={styles.textMain}>Transaction History</Text>
-          </TouchableHighlight>
+          </TouchableOpacity>
         </View>
 
         {/* New subset of information */}
@@ -252,7 +307,7 @@ export default class ProfileScreen extends Component {
         </View>
 
         <View>
-          <Text style ={styles.textMain}>Choose Your Stylists</Text>
+          <Text style ={styles.textMain}>Meet Your Stylists</Text>
         </View>
 
         <View style={{flexDirection:'row', justifyContent: 'center', marginTop: 10}}>
@@ -263,7 +318,7 @@ export default class ProfileScreen extends Component {
                 style ={styles.iconImage}
                 source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/your_profile_picture.jpg')}
               />
-              <Text>Mona Fang</Text>
+              <Text style={styles.textLittle}>Mona Fang</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
@@ -273,7 +328,7 @@ export default class ProfileScreen extends Component {
                 style ={styles.iconImage}
                 source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/anahita.jpg')}
               />
-              <Text>Anahita</Text>
+              <Text style={styles.textLittle}>Anahita</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
@@ -283,7 +338,7 @@ export default class ProfileScreen extends Component {
                 style ={styles.iconImage}
                 source={require('/Users/nn121local/Documents/GitHub/BusinessINCsoftware/my-app/assets/images/leilani.jpg')}
               />
-              <Text>Leilani</Text>
+              <Text style={styles.textLittle}>Leilani</Text>
           </TouchableOpacity>
         </View>
 
